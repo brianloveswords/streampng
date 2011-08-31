befa5411fbf8ea5675f93f60913770f60ecad9d6
Brian J Brennan <brianloveswords@gmail.com>
2011-08-31 15:11:03 -0700

    Docs docs docs.

--- metapng.js
+++ metapng.js
@@ -1,6 +1,10 @@
 var fs = require('fs')
   , util = require('./lib/util.js')
 
+// ByteArray is a janky implementation of array that allows me to pretend I
+// have a typed array. It can turn four bytes into an unsigned 32 bit integer
+// (big-endian). Once a stable version of node comes out with typed arrays,
+// replace this with that.
 var ByteArray = function(bytes){
   if (!(this instanceof ByteArray)) return new ByteArray(bytes);
   if (bytes) { this.pushBytes(bytes); }
@@ -23,6 +27,9 @@ ByteArray.prototype.pushBytes = function(bytes) {
 }
 
 const PNG_MAGIC_NUMBER = ByteArray([137, 80, 78, 71, 13, 10, 26, 10]);
+
+// An interface for reading chunks from a PNG. `source` can be a buffer, file
+// descriptor or a string containing the path to a file.
 var Reader = function(source) {
   if (!(this instanceof Reader)) return new Reader(source);
   this.source = source;
@@ -61,6 +68,10 @@ Reader.prototype.peek = function(len) {
   if (!this.data) this.getContents();
   return this.data.slice(this.cursor, (this.cursor + len));
 }
+
+// Ensures that the buffer being read is a PNG by comparing it to the known
+// PNG magic number, then read all of the chunks in the file to an array
+// `this.chunks`.
 Reader.prototype.readChunks = function() {
   var magic_nom_nom
     , chunk;
@@ -71,9 +82,11 @@ Reader.prototype.readChunks = function() {
   while (chunk = this.readNextChunk()) this.chunks.push(chunk)
   return this.chunks;
 }
+// Generally this should not be called by itself, `this.readChunks` should be
+// used. If `this.cursor` is not at a chunk boundary, this should probably cause
+// an error. Otherwise there's a chance that this can silently return garbage data.
 Reader.prototype.readNextChunk = function() {
   if (this.cursor === this.data.length) return null;
-  // order is important here.
   var start = this.cursor
     , len = ByteArray(this.eat(4)).to32Int()
     , type = this.eat(4).toString()
@@ -82,6 +95,10 @@ Reader.prototype.readNextChunk = function() {
     , end = this.cursor
   return {start: start, len: len, type: type, data: data, crc: crc, end: end}
 }
+// Get an array of chunks that match the type. Possible types are listed here:
+// http://www.w3.org/TR/PNG/#11Chunks. Order is determined by where the chunks
+// appear in the file, and the return type will always be an array (even if
+// the specification only allows one one instance of that type)
 Reader.prototype.findByType = function(type) {
   if (!this.chunks) this.readChunks()
   return this.chunks.filter(function(chunk){
@@ -90,6 +107,12 @@ Reader.prototype.findByType = function(type) {
 }
 
 
+// Writer is an interface for inserting chunks into a PNG. Other than
+// generating a valid, CRCd chunk, there is no validation to speak of. Using this
+// to add types with invalid data can break your PNG.
+//
+// Using pushBytes in this way causes five new buffers to be created. This can
+// definitely be done more efficiently.
 function Writer(source) {
   if (!(this instanceof Writer)) return new Writer(source);
   Reader.call(this, source)
@@ -108,11 +131,16 @@ Writer.prototype.chunk = function(type, data) {
 
   return Buffer(chunk);
 }
+
+// The specification for tEXt chunks requires a keyword and data, separated by
+// a null character, for the body of the chunk.
 Writer.prototype.tEXt = function(keyword, data) {
   return this.chunk('tEXt', [keyword, data].join("\u0000"));
 }
 
+// Expose objects as well as shortcut methods for reading and writing tEXt chunks.
 exports.Reader = Reader;
+exports.Writer = Writer;
 exports.read = function(src, key){
   var textChunks = Reader(src).findByType('tEXt');
   return textChunks;
