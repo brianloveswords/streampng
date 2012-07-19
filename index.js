35162215d188c4225bd2bb6aa391188ee9de93a0
Brian J Brennan <brianloveswords@gmail.com>
2012-07-19 12:45:54 -0400

    Throw error on construction failure.

--- index.js
+++ index.js
@@ -27,17 +27,24 @@ function StreamPng(input) {
   // Input can be either a buffer or a stream. When we get a buffer, we can
   // pretend it's a stream by writing the entire buffer at once, as if we just
   // got a single `data` event. If it's not a buffer, check whether input
-  // quacks like a stream and pipe it back to this instance. Otherwise emit
-  // an error. We don't want to throw an error because the object is still
-  // salvageable.
+  // quacks like a stream and pipe it back to this instance. Otherwise throw
+  // an error.
   if (input) {
+
+    // is it a buffer?
     if (Buffer.isBuffer(input))
-      this.write(input);
+      this.end(input);
+
+    // does it quack like a stream?
     else if (typeof input.pipe === 'function')
       input.pipe(this);
+
+    // can't handle it.
     else {
       var err = new TypeError('PNG constructor takes either a buffer or a stream');
-      this.emit('error', err);
+      err.input = input;
+      err.inputType = typeof input;
+      throw err;
     }
   }
 }
